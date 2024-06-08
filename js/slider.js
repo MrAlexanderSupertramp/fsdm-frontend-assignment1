b1763f2522533486c60296c10dcd72b8a1473ea3
Jiril Zala <zjiril@gmail.com>
2024-06-07 21:23:25 -0400

    deploy

---
+++ js/slider.js
@@ -0,0 +1,40 @@
+let slideIndex = 1;
+            showSlides(slideIndex);
+            
+            function plusSlides(n) {
+              showSlides(slideIndex += n);
+            }
+            
+            function currentSlide(n) {
+              showSlides(slideIndex = n);
+            }
+            
+            function showSlides(n) {
+
+                let i;
+
+                let slides = document.getElementsByClassName("mySlides");
+
+                let dots = document.getElementsByClassName("dot"); 
+
+                if (n < 1) {slideIndex = slides.length}
+                
+                if (n > slides.length) {slideIndex = 1}   
+
+                for (i = 0; i < dots.length; i++) {
+                
+                    dots[i].className = dots[i].className.replace(" active", "");
+
+                }
+
+                for (i = 0; i < slides.length; i++) {
+                
+                slides[i].style.display = "none";  
+
+                }
+
+                dots[slideIndex-1].className += " active";
+
+                slides[slideIndex-1].style.display = "block";  
+
+            }
